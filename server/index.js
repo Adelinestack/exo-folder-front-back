51ffd3eae0b2dd5b4072dcf45093a2b270ed0d60
Adelinestack <adeline.colombo@stackerine.com>
2019-02-21 14:26:11 +0100

    server allow to display files and folders

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-const server = app.listen(3000, function() {
-  console.log(`Server started on http://localhost:${server.adress().port}`);
+app.use('/', express.static('../client/build/'));
+
+const folderContentByPath = folderPath => {
+  return fs.readdirSync(`../foldertest${folderPath}`, { withFileTypes: true });
+};
+
+const fileContentByPath = filePath => {
+  return fs.readFileSync(`../foldertest${filePath}`, 'utf8');
+};
+
+app.get('/getFolder', (req, res) => {
+  const path = req.query.folderPath;
+  if (fs.statSync(`../foldertest${path}`).isFile()) {
+    const contentFile = fileContentByPath(path);
+    res.send([{ content: contentFile }]);
+  } else {
+    const readFolder = folderContentByPath(path);
+    const contentFolder = readFolder.map(element => ({
+      name: element.name,
+      isDir: element.isDirectory(),
+    }));
+    res.send(contentFolder);
+  }
+});
+
+const server = app.listen(8080, (req, res) => {
+  console.log(`server started on ${server.address().port}`);
 });

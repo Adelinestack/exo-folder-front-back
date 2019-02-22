ffe6c13dfe7e5c4368d6d1ef648766d4300c5a7a
Adelinestack <adeline.colombo@stackerine.com>
2019-02-22 09:59:16 +0100

    server delete file and folder, create file without refreshing page

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const rimraf = require('rimraf');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', express.static('../client/build/'));
 
@@ -10,6 +14,23 @@ const folderContentByPath = folderPath => {
 
 const fileContentByPath = filePath => {
   return fs.readFileSync(`../foldertest${filePath}`, 'utf8');
+};
+
+const deleteElement = (elementPath, elementType) => {
+  if (elementType === 'File') {
+    return fs.unlinkSync(`../foldertest${elementPath}`);
+  } else {
+    return rimraf.sync(`../foldertest${elementPath}`, { optn: 'rmdirSync' });
+  }
+};
+
+const createFile = (filePath, fileName) => {
+  console.log(`../foldertest${filePath}/${fileName}`);
+  fs.appendFileSync(
+    `../foldertest${filePath}/${fileName}`,
+    'New file for test',
+    'utf8'
+  );
 };
 
 app.get('/getFolder', (req, res) => {
@@ -25,6 +46,26 @@ app.get('/getFolder', (req, res) => {
     }));
     res.send(contentFolder);
   }
+});
+
+app.delete('/getFolder', (req, res) => {
+  const path = req.query.elementPath;
+  const type = req.query.elementType;
+
+  const isElementDeleted = deleteElement(path, type);
+  if (typeof isElementDeleted === 'undefined') {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/getFolder', (req, res) => {
+  console.log(req.body);
+  const path = req.body.filePath;
+  const fileName = req.body.fileName;
+  createFile(path, fileName);
+  res.sendStatus(200);
 });
 
 const server = app.listen(8080, (req, res) => {
